8dd3f18d788fabc57ba84cf9bf85375333804a24
Daniel David <dannieimeh@gmail.com>
2025-07-16 08:47:46 +0100

    hero nearly done

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,17 +4,19 @@ import bakedBerserk from '../assets/bakedberserkk.png';
 
 const Hero = () => {
 	return (
-		<main className='w-[80%] h-[80%] my-0 mx-auto flex items-center justify-center spaceGrotesk'>
-			<section className='flex items-center justify-center w-full h-full my-[1%] mx-auto text-white'>
+		<main className='w-[85%] sm:w-[80%] h-[80%] my-0 mx-auto flex items-center justify-center spaceGrotesk'>
+			<section className='flex items-center md:flex-row justify-center w-full h-full my-[1%] mx-auto text-white flex-col '>
 				<section className='basis-[45%] '>
-					<h1 className='text-[3.125rem] font-bold mb-4'>What is NairaX?</h1>
+					<h1 className='text-[1.875rem] mb-6 sm:text-[3.125rem] font-bold sm:mb-4'>
+						What is NairaX?
+					</h1>
 
-					<p className='text-[1.125rem] font-medium mb-4'>
+					<p className='text-xs mb-6 sm:text-[1.125rem] font-medium sm:mb-4'>
 						NairaX is a decentralized exchange platform that allows users to
 						trade cryptocurrencies in a secure and efficient manner.
 					</p>
 
-					<div className='flex items-center justify-start gap-6 mb-4'>
+					<div className='flex items-center justify-between w-[95.5%]  md:justify-start gap-6 mb-6 md:mb-4'>
 						<button>buy now</button>
 
 						<button className='border-2 border-secondary bg-transparent'>
@@ -22,7 +24,7 @@ const Hero = () => {
 						</button>
 					</div>
 
-					<div className='flex items-center justify-start gap-4'>
+					<div className='flex items-center justify-start gap-[1.95rem] md:gap-4'>
 						<h3 className='text-[2.5rem] flex flex-col'>
 							4K+ <span className='text-xs mt-[-0.5rem]'>collections</span>
 						</h3>
@@ -35,31 +37,31 @@ const Hero = () => {
 					</div>
 				</section>
 
-				<section className='flex items-center justify-center basis-[55%] h-[45vh]'>
-					<div className='relative mr-[-7rem] rotate-[-15deg] '>
-						<div className='absolute w-[13.0625rem] h-full bg-[#ffffffcb] filter blur-[3px] '></div>
+				<section className='flex items-center justify-center basis-[55%] sm:basis-[50%] sm:w-full h-[45vh] mt-[17%]'>
+					<div className='relative mr-[-4rem] md:mr-[-7rem] rotate-[-15deg] '>
+						<div className='absolute w-[9.089375rem] md:w-[13.0625rem] h-full bg-[#ffffffcb] filter blur-[3px] '></div>
 						<img
 							src={coralTribe}
 							alt='coraltribe'
-							className='h-[13.0625rem] w-[13.0625rem] relative p-4'
+							className='h-[13.0625rem] w-[9.089375rem] md:w-[13.0625rem] relative p-4'
 						/>
 					</div>
 
 					<div className='relative mt-[-4.5rem] z-10'>
-						<div className='absolute w-[13.0625rem] h-full bg-[#ffffffcb] filter blur-[3px]'></div>
+						<div className='absolute w-[9.089375rem] md:w-[13.0625rem] h-full bg-[#ffffffcb] filter blur-[3px]'></div>
 						<img
 							src={spaceWarlock}
 							alt='spaceWarlock'
-							className='h-[13.0625rem] w-[13.0625rem] relative p-4'
+							className='h-[13.0625rem] w-[9.089375rem] md:w-[13.0625rem] relative p-4'
 						/>
 					</div>
 
-					<div className='relative ml-[-7rem] rotate-[15deg] '>
-						<div className='absolute w-[13.0625rem] h-full bg-[#ffffffcb] filter blur-[3px]'></div>
+					<div className='relative ml-[-4rem] md:ml-[-7rem] rotate-[15deg] '>
+						<div className='absolute w-[9.089375rem] md:w-[13.0625rem] h-full bg-[#ffffffcb] filter blur-[3px]'></div>
 						<img
 							src={bakedBerserk}
 							alt='bakedBerserk'
-							className='h-[13.0625rem] w-[13.0625rem] relative p-4'
+							className='h-[13.0625rem] w-[9.089375rem] md:w-[13.0625rem] relative p-4'
 						/>
 					</div>
 				</section>
